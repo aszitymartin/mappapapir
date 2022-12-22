591e8c64670030c3a75d4a8e64391bb18a0f6335
Martin Aszity <martinaszity@icloud.com>
2022-12-22 14:53:57 +0100

    Checkout -> Megrendelés leadása hiányos kitöltés esetén

--- webshop/checkout.php
+++ webshop/checkout.php
@@ -379,12 +379,12 @@
                         <input id="re-${emptyRequiredFields[i].id}" type="${emptyRequiredFields[i].type}" class="re-generated-checkout-field w-fa text-primary border-soft background-bg padding-1-05 outline-none border-none" placeholder="${emptyRequiredFields[i].placeholder}" autocomplete="${emptyRequiredFields[i].autocomplete}">
                     </div>
                     `;
-                }
-                $(".re-generated-checkout-field").keyup(function() {
-                    var $emptyFields = $('.re-generated-checkout-field').filter(function() { return $.trim(this.value) === ""; });
-                    if (!$emptyFields.length) {
-                        document.getElementById('ch-ea-rt-bt').classList.replace('not-allowed', 'pointer');
-                        document.getElementById('ch-ea-rt-bt').classList.add('primary-bg-hover', 'splash');
+                } var rod = { ...orderData }; var caller = false;
+                $(".re-generated-checkout-field").keyup(function() { var $emptyFields = $('.re-generated-checkout-field').filter(function() { return $.trim(this.value) === ""; });
+                    if (!$emptyFields.length) { caller = true; callCheckOutReSubmit(caller, rod);
+                        document.getElementById('ch-ea-rt-bt').classList.replace('not-allowed', 'pointer'); document.getElementById('ch-ea-rt-bt').classList.add('primary-bg-hover', 'splash');
+                    } else { caller = false; callCheckOutReSubmit(caller);
+                        document.getElementById('ch-ea-rt-bt').classList.replace('pointer', 'not-allowed'); document.getElementById('ch-ea-rt-bt').classList.remove('primary-bg-hover', 'splash');
                     }
                 });
             }
@@ -429,6 +429,41 @@
                 $('#cl__ebox').click(function () { ce__box.classList.replace("popup", "popout"); ce__wrapper.classList.add("fadeout"); setTimeout(() => {ce__wrapper.remove(); $("#tab-credit").load('/includes/profile/credit.php'); $('html').css("overflow-y", "unset");},235); });
             }
         });
+    } var reSubmitCalled = false;
+    function reSubmitCheckOutPost (o) {
+        if (!reSubmitCalled) {
+            document.getElementById('ch-tb-cn').innerHTML = `
+                <div class="flex flex-col flex-align-c flex-justify-con-c gap-1 small text-muted user-select-none w-fa">
+                    <span><svg class='wizard_input_loading' xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g><polygon points="0 0 24 0 24 24 0 24"/></g><path d="M12,4 L12,6 C8.6862915,6 6,8.6862915 6,12 C6,15.3137085 8.6862915,18 12,18 C15.3137085,18 18,15.3137085 18,12 C18,10.9603196 17.7360885,9.96126435 17.2402578,9.07513926 L18.9856052,8.09853149 C19.6473536,9.28117708 20,10.6161442 20,12 C20,16.418278 16.418278,20 12,20 C7.581722,20 4,16.418278 4,12 C4,7.581722 7.581722,4 12,4 Z" class="svg" fill-rule="nonzero" opacity="0.4" transform="translate(12.000000, 12.000000) scale(-1, 1) translate(-12.000000, -12.000000) "/></g></svg></span>
+                    <span>Adatok feldolgozása</span>
+                </div>
+            `;
+            var postData = new FormData(); postData.append("data", JSON.stringify(o));
+            $.ajax({ enctype: "multipart/form-data", type: "POST", url: "/webshop/includes/checkout/placeOrder.php", data: postData, dataType: 'json', contentType: false, processData: false,
+                success: function(data) {
+                    console.log(data);
+                }, error: function (data) { console.log(data); }
+            });
+
+
+        } else { callCheckOutReSubmit(false); }
+    } function reSubmitCheckOut (od) {
+        // orderData objektum manipulalasa a hianyzo adatok alapjan
+        var ri = document.getElementsByClassName('re-generated-checkout-field'); var rid = [];
+        for (let i = 0; i < ri.length; i++) { rid.push(ri[i].id); }
+        for (let i = 0; i < rid.length; i++) {
+            for (let j = 0; j < rid[i].split('re-ch-')[1].length; j++) {
+                if (rid[i].split('re-ch-')[1].split('-').length > 1) {
+                    if (rid[i].split('re-ch-')[1].split('-')[0] == 'inv') { od.invoice[rid[i].split('re-ch-')[1].split('-')[1]] = document.getElementById(rid[i]).value; }
+                    if (rid[i].split('re-ch-')[1].split('-')[0] == 'shp') { od.shipping[rid[i].split('re-ch-')[1].split('-')[1]] = document.getElementById(rid[i]).value; }
+                } else { od.general[rid[i].split('re-ch-')[1].split('-')] = document.getElementById(rid[i]).value; }
+            }
+        } reSubmitCheckOutPost(od); reSubmitCalled = true;
+    } function callCheckOutReSubmit (caller, o = 0) {
+        if (caller == true) { var eventHandler = function () { reSubmitCheckOut(o); };
+            document.getElementById('ch-ea-rt-bt').addEventListener('click', eventHandler, false);
+        } else { document.getElementById('ch-ea-rt-bt')?.classList.replace('pointer', 'not-allowed'); document.getElementById('ch-ea-rt-bt')?.classList.remove('primary-bg-hover', 'splash'); $('#ch-ea-rt-bt').off(); $('#ch-ea-rt-bt').unbind(); document.getElementById('ch-ea-rt-bt')?.removeEventListener("click", eventHandler , false); }
     }
+
 </script>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'); ?>
